24205a96d425d6b73b413f085c5599bb7c0dd251
Hunter Sherman <hunter.sherman@vantagepointanalytics.com>
2016-02-09 16:05:42 -0600

    Update caskconfig.sh

--- caskconfig.sh
+++ caskconfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-xcode-select –install
-ruby -e “$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)”
+xcode-select --install
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install caskroom/cask/brew-cask
 brew cask install google-chrome
 brew cask install intellij-idea
